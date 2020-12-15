e9e89eb81f1e239106169fcea1bd626542910194
HY NGUYEN <nguyenminhhy>
2020-12-15 09:54:49 +0700

    file upload

--- models/book.js
+++ models/book.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const coverImageBasePath = 'uploads/bookCovers'
-const path = require('path')
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -22,7 +20,11 @@ const bookSchema = new mongoose.Schema({
         required: true,
         default: Date.now
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true
+    },
+    coverImageType: {
         type: String,
         required: true
     },
@@ -33,11 +35,10 @@ const bookSchema = new mongoose.Schema({
     }
 })
 
-bookSchema.virtual('coverImagePath').get(function(){
-    if(this.coverImageName != null){
-        return path.join('/', coverImageBasePath, this.coverImageName)
+bookSchema.virtual('coverImagePath').get(function() {
+    if (this.coverImage != null && this.coverImageType != null) {
+      return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
     }
-})
+  })
 
 module.exports = mongoose.model('Book', bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
